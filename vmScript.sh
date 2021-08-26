75be71360a1c2129f84656b72779ddd761236e71
dsetareh <dsetareh@vt.edu>
2021-08-26 01:11:43 +0000

    vm script

---
+++ vmScript.sh
@@ -0,0 +1,13 @@
+apt update && apt -y upgrade
+apt install -y build-essential cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev libzmq3-dev
+git clone https://github.com/dsetareh/srsRAN
+ip netns add ue1
+cd srsRAN
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+make
+cd ..
+tar xvf currConfs.tar
+cd currconf/
+mkdir /etc/srsran
+cp ./* /etc/srsran/
